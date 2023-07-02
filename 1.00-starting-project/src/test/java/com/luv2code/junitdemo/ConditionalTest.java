f71d23dec8a67a76688f89a71a4442d063905be6
Yashwanth Anand S <yashwanthanands@outlook.com>
2023-07-02 18:42:01 +0530

    EnabledOnOS Test annotation implemented

--- 1.00-starting-project/src/test/java/com/luv2code/junitdemo/ConditionalTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/ConditionalTest.java
@@ -1,16 +1,26 @@
 package com.luv2code.junitdemo;
 
+import jdk.jfr.Enabled;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 /**
  * @author yashwanthanands
  */
 public class ConditionalTest {
 
+    
     @Test
     @Disabled("Dont run until JIRA #123 is resolved")
     void basicTest() {
+        // execute method and perform asserts
+    }
+
+    @Test
+    @EnabledOnOs(OS.WINDOWS)
+    void testforWindowsOnly() {
         // execute method and perform asserts
     }
 }
